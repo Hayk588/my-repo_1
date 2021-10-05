42406123adf83e6297a6b9f0688b558b85e5cc77
Hrach Papikyan <hrach.papikyan@deloitte-audit-analytics.com>
2021-10-05 13:25:28 +0400

    Initial commit

---
+++ src/chapter3/Lesson3.java
@@ -0,0 +1,117 @@
+package chapter3;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+//        int lightspeed;
+//        long days;
+//        long seconds;
+//        long distance;
+//
+//        lightspeed = 186000;
+//
+//        days = 1000;
+//
+//        seconds = days * 24 * 60 * 60;
+//
+//        distance = lightspeed * seconds;
+//
+//        System.out.print("Зa " + days);
+//        System.out.print(" дней свет пройдет около ");
+//        System.out.println(distance + " миль.");
+
+
+// =====================================================================
+
+
+//        double pi, r, a;
+//
+//        r = 10.8;
+//        pi = 3.1416;
+//        a = pi * r * r;
+//
+//        System.out.println("Плoщaдь круга равна " + a);
+
+
+// ====================================================================
+
+//        char ch1, ch2;
+//
+//        ch1 = 88;
+//        ch2 = 'Y';
+//
+//        System.out.print("chl и ch2: ");
+//        System.out.println(ch1 + " " + ch2);
+
+//
+//        char ch1;
+//
+//        ch1 = 'X';
+//        System.out.println("ch1 содержит " + ch1);
+//
+//        ch1++;
+//        System.out.println("ch1 теперь содержит " + ch1);
+
+
+// =======================================================================
+
+//        boolean b = false;
+//        System.out.println("b равно " + b);
+//        b = true;
+//        System.out.println("b равно " + b);
+//
+//        if (b) {
+//            System.out.println("Этoт код выполняется");
+//        }
+//
+//        b = false;
+//        if (b) {
+//            System.out.println("Этoт код не выполняется.");
+//        }
+//
+//        System.out.println("10 > 9 равно " + (10 > 9));
+
+        //===============================================================
+
+
+        //Erkuakan literal
+        //      int x = 0b1010;
+
+
+//==========================================================================
+
+//        byte b;
+//        int i = 257;
+//        double d = 323.142;
+//
+//        System.out.println("Пpeoбpaзoвaниe типа int в тип byte.");
+//        b = (byte) i;
+//        System.out.println("i и b" + i + " " + b);
+//
+//        System.out.println("Пpeoбpaзoвaниe типа double в тип int.");
+//
+//        i = (int) d;
+//        System.out.println("d и i " + d + " " + i);
+//
+//        System.out.println("Пpeoбpaзoвaниe типа double в тип byte.");
+//        b = (byte) d;
+//        System.out.println("d и b " + d + " " + b);
+
+
+//        byte b = 42;
+//        char c = 'a';
+//        short s = 1024;
+//        int i = 50000;
+//        float f = 5.67f;
+//        double d = .1234;
+//        double result = (f * b) + (i / c) - (d * s);
+//        System.out.println((f * b) + " + " + (i / c) + " - " + (d * s));
+//        System.out.println("result = " + result);
+
+//
+//        char ch;
+//        for (ch = 'X'; ch < 100; ch++) {
+//            System.out.println(ch);
+//        }
+
+    }
+}
